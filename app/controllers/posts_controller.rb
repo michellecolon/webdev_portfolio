01a8c18f884a69eba39f09ba31896862fce67f58
Michelle Colon <michellecolon.mc@gmail.com>
2016-04-01 00:34:10 -0400

    Styling on 33016

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,49 +1,50 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show, :edit, :update, :destroy]
+before_action :find_post, only: [:show, :edit, :update, :destroy]
 
-  def index
-  end
+def index
+end
 
-  def new
-    @post = Post.new
-  end
+def new
+  @post = Post.new
+end
 
-  def create
-    @post = Post.new post_params
+def create
+  @post = Post.new post_params
 
-    if @post.save
-      redirect_to @post, notice: "Your post has been successfully saved!"
-    else
-      render 'new', notice: "Oh no, your post wasn't saved :("      
-    end
-  end
+  if @post.save
+    redirect_to @post, notice: "Your post was successfully saved!"
+  else
+    render 'new', notice: "Oh no, your post didn't save :("
+  end   
+end
 
-  def show
-  end
+def show
+end
 
-  def edit
-  end
+def edit
+end
 
-  def update
-    if @post.update post_params
-      redirect_to @post, notice: "Huzzah! You updated the post!"
-    else
-      render 'edit'
-    end
-  end
+def update
+  if @post.update post_params
+    redirect_to @post, notice: "Your post was successfully saved!"
+  else
+    render 'edit'
+  end  
+end
 
-  def destroy
-    @post.destroy
-    redirect_to posts_path
-  end
+def destroy
+  @post.destroy
+  redirect_to posts_path
+end
 
-  private
+private
 
-  def post_params
-    params.require(:post).permit(:title, :content)
-  end
+def post_params
+  params.require(:post).permit(:title, :content)
+end
 
-  def find_post
-    @post = Post.find(params[:id])
-  end
+def find_post
+  @post = Post.find(params[:id])
+end
+
 end
