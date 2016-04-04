166b7ecb82abd8a4429cd809ffdb5bad89b00efa
Michelle Colon <michellecolon.mc@gmail.com>
2016-04-04 01:58:12 -0400

    Add edit & destroy project abilities

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,7 +19,22 @@ class ProjectsController < ApplicationController
   end
 
   def show
+  end
 
+  def edit
+  end
+
+  def update
+    if @project.update project_params
+      redirect_to @project, notice: "You just successfully updated your blog post!"
+    else
+      render 'edit'
+    end  
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
   end
 
   private
