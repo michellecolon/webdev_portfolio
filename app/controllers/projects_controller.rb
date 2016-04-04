82ec1f99e80e35f913edce1b7e8f06f5589c0e0f
Michelle Colon <michellecolon.mc@gmail.com>
2016-04-04 01:42:05 -0400

    Setup friendly_id for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -25,10 +25,10 @@ class ProjectsController < ApplicationController
   private
 
   def find_project
-    @project = Project.find(params[:id])
+    @project = Project.friendly.find(params[:id])
   end
 
   def project_params
-    params.require(:project).permit(:title, :content)
+    params.require(:project).permit(:title, :content, :slug)
   end
 end
